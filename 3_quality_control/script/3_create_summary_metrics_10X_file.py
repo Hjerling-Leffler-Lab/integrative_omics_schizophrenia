9c9ccaa8ed9f3757eef18d35f2763a69a1b08d0d
LisaBast <32566628+LisaBast@users.noreply.github.com>
2026-02-05 20:20:25 +0100

    renaming
    rename tables and ID column variables

--- 3_quality_control/script/3_create_summary_metrics_10X_file.py
+++ 3_quality_control/script/3_create_summary_metrics_10X_file.py
@@ -2,7 +2,7 @@
 #version: 0.2.0
 #Created on Fri Oct  9 14:47:39 2020
 #@author: Lisa Bast
-#extract QC metrics from several sources (10X summary metrics, filtering result, stats based on raw cound data) and summarize them in a dataframe/ .csv file
+#extract QC metrics from several sources (10X summary metrics, filtering result, stats based on raw count data) and summarize them in a dataframe/ .csv file (exports table T3: T3_quality_metrics_per_sample.xlsx)
 
 
 import os
@@ -30,18 +30,16 @@ opt_concatenate_metrics_summary=False#True
 opt_integrate_filtering_results=True
 opt_integrate_raw_data_stats=True
 
-os.chdir(path_data)
-df = pd.read_csv('metrics_summary_concatenated.csv')
+#read metrics exported from 10X summary
+df = pd.read_csv(path_data+'metrics_summary_concatenated.csv')
 
 # add IDs to df
 df_IDs = pd.read_excel(path_data+"T1_basic_donor_information.xlsx", "basic_donor_information", engine='openpyxl') 
-df_IDs.rename(columns = {'donor_ID_internal_7':'donor_ID_library'},inplace=True)
-df_IDs.rename(columns = {'donor_ID_internal_8':'donor_ID_python'},inplace=True)
 df_IDs.rename(columns = {'scz_status':'Group'},inplace=True)
 df_IDs.loc[df_IDs["Group"]=='scz']["Group"]="SCZ"
 df_IDs.loc[df_IDs["Group"]=='ctrl']["Group"]="CTRL"
-df_IDs['Library'] = df_IDs['donor_ID_library'].str[:4]
-df_metrics = pd.merge(df_IDs[['donor_ID_library', 'donor_ID_universal','donor_ID_python','Group','Library']],df,on='donor_ID_library')
+df_IDs['Library'] = df_IDs['Internal_donor_batch_ID'].str[:4]
+df_metrics = pd.merge(df_IDs[['Internal_donor_batch_ID', 'donor_ID_universal','Internal_donor_ID','Group','Library']],df,on='Internal_donor_batch_ID')
 df_metrics['p_unmapped_reads'] = 100-df_metrics['Fraction Reads in Cells'].str[:-1].astype(float)
 
 #rename important columns
@@ -85,16 +83,16 @@ df_metrics.to_csv(path_output+'metrics_summary_tidy.csv',index=False)
 if opt_integrate_filtering_results:
     #% cells removed in filtering
     df_filt_result_pagoda = pd.read_pickle(path_output+"filtered/df_pagoda_filtering_result_cellranger")
-    df_filt_result_pagoda.rename(columns = {'Donor ID':'donor_ID_python','Percentage Cells discarded':'p_cells_del_filt_pagoda'},inplace=True)
+    df_filt_result_pagoda.rename(columns = {'Donor ID':'Internal_donor_ID','Percentage Cells discarded':'p_cells_del_filt_pagoda'},inplace=True)
     #to do: calculate number of cells prior to any filtering based on pagoda df
     df_filt_result_pagoda["Number Cells prior filtering"] = ((df_filt_result_pagoda["Number of cells removed (pagoda filtering)"]*100)/df_filt_result_pagoda["p_cells_del_filt_pagoda"])
     #merge with other filtering data frames:
     df_filt_result_TH = pd.read_pickle(path_output+"filtered/df_pagoda_TH_filtering_result_cellranger")
-    df_filt_result_TH.rename(columns = {'Donor ID':'donor_ID_python','Percentage Cells discarded':'p_cells_del_filt_TH'},inplace=True)
+    df_filt_result_TH.rename(columns = {'Donor ID':'Internal_donor_ID','Percentage Cells discarded':'p_cells_del_filt_TH'},inplace=True)
     df_filt_result_D = pd.read_pickle(path_output+"filtered/df_pagoda_TH_and_D_adj_filtering_result_cellranger")
-    df_filt_result_D.rename(columns = {'Donor ID':'donor_ID_python','Percentage Cells discarded':'p_cells_del_filt_D'},inplace=True)
-    df_filt = pd.merge(df_filt_result_pagoda,df_filt_result_TH,on=['donor_ID_python',"Disease"],how='left')
-    df_filt = pd.merge(df_filt, df_filt_result_D,on=['donor_ID_python',"Disease"],how='left')
+    df_filt_result_D.rename(columns = {'Donor ID':'Internal_donor_ID','Percentage Cells discarded':'p_cells_del_filt_D'},inplace=True)
+    df_filt = pd.merge(df_filt_result_pagoda,df_filt_result_TH,on=['Internal_donor_ID',"Disease"],how='left')
+    df_filt = pd.merge(df_filt, df_filt_result_D,on=['Internal_donor_ID',"Disease"],how='left')
     # add numbers of cells removed/ kept in each filtering step
     df_filt['Number of cells post pagoda filtering'] = df_filt['Number Cells prior filtering']-df_filt["Number of cells removed (pagoda filtering)"]
     df_filt['Number of cells removed (TH filtering)'] = (df_filt["p_cells_del_filt_TH"]/100)*df_filt["Number of cells post pagoda filtering"]
@@ -104,12 +102,12 @@ if opt_integrate_filtering_results:
     df_filt["p_cells_del_filt"] = (df_filt_result_pagoda["Number Cells prior filtering"]-df_filt['Number of cells post D filtering'])/df_filt_result_pagoda["Number Cells prior filtering"]
 
     #merge filtering result with other metrics data frame
-    df_metrics1 = pd.merge(df_metrics,df_filt[['p_cells_del_filt','donor_ID_python']],on='donor_ID_python',how='left')
+    df_metrics1 = pd.merge(df_metrics,df_filt[['p_cells_del_filt','Internal_donor_ID']],on='Internal_donor_ID',how='left')
 
 #read patient-level data
 df_patientData = pd.read_excel(path_data+'T1_basic_donor_information.xlsx', 'sample_info',engine='openpyxl') 
-df_patientData.rename(columns = {'donor_ID_internal_8':'donor_ID_python'},inplace=True)
-df_metrics = pd.merge(df_metrics1,df_patientData[['gender','age','PMI_h','donor_ID_python']],on='donor_ID_python',how='left')
+df_patientData.rename(columns = {'donor_ID_internal_8':'Internal_donor_ID'},inplace=True)
+df_metrics = pd.merge(df_metrics1,df_patientData[['gender','age','PMI_h','Internal_donor_ID']],on='Internal_donor_ID',how='left')
 
 #transform PMI
 PMI = df_metrics['PMI_h'].astype('str')
@@ -124,8 +122,8 @@ df_metrics['PMI_h']=PMI_n.astype('float')
 if opt_integrate_raw_data_stats:
     #std_reads_per_umi
     df_raw_stats = pd.read_pickle(path_output + "raw/df_stats")
-    df_raw_stats.rename(columns = {'Mean counts per barcode':'mean_counts_per_barcode','Std counts per barcode':'std_counts_per_barcode','Median genes per cell':'median_gpc','Donor ID':'donor_ID_python'},inplace=True)
-    df_metrics = pd.merge(df_metrics,df_raw_stats[['mean_counts_per_barcode','std_counts_per_barcode','median_gpc','donor_ID_python']],on='donor_ID_python',how='left')
+    df_raw_stats.rename(columns = {'Mean counts per barcode':'mean_counts_per_barcode','Std counts per barcode':'std_counts_per_barcode','Median genes per cell':'median_gpc','Donor ID':'Internal_donor_ID'},inplace=True)
+    df_metrics = pd.merge(df_metrics,df_raw_stats[['mean_counts_per_barcode','std_counts_per_barcode','median_gpc','Internal_donor_ID']],on='Internal_donor_ID',how='left')
 
 if opt_add_neuron_percentage == True:
     os.chdir(path_main + '/4_data_integration_and_cell_type_annotation/output/15_CTs/')
@@ -143,19 +141,19 @@ if opt_add_neuron_percentage == True:
                 n_neurons = n_nuclei
             #create dataframe with sample_ID and n_cells and neuronal status:
             if bool_first:
-                df_neuron_percentage = pd.DataFrame(data = {'donor_ID_python': sample_IDs, 'n_cells': n_cells, 'n_neurons': n_neurons})
+                df_neuron_percentage = pd.DataFrame(data = {'Internal_donor_ID': sample_IDs, 'n_cells': n_cells, 'n_neurons': n_neurons})
                 bool_first=False
             else:
                 for s in sample_IDs:
-                    df_neuron_percentage['n_cells'][df_neuron_percentage['donor_ID_python']==s] = df_neuron_percentage['n_cells'][df_neuron_percentage['donor_ID_python']==s].copy()+n_cells[sample_IDs==s][0]
-                    df_neuron_percentage['n_neurons'][df_neuron_percentage['donor_ID_python']==s] = df_neuron_percentage['n_neurons'][df_neuron_percentage['donor_ID_python']==s].copy()+n_neurons[sample_IDs==s][0]
+                    df_neuron_percentage['n_cells'][df_neuron_percentage['Internal_donor_ID']==s] = df_neuron_percentage['n_cells'][df_neuron_percentage['Internal_donor_ID']==s].copy()+n_cells[sample_IDs==s][0]
+                    df_neuron_percentage['n_neurons'][df_neuron_percentage['Internal_donor_ID']==s] = df_neuron_percentage['n_neurons'][df_neuron_percentage['Internal_donor_ID']==s].copy()+n_neurons[sample_IDs==s][0]
     df_neuron_percentage['p_neurons'] = (df_neuron_percentage['n_neurons']/df_neuron_percentage['n_cells'])*100
-    df_metrics_new = pd.merge(df_metrics,df_neuron_percentage[['p_neurons','donor_ID_python']],on='donor_ID_python',how='left')
+    df_metrics_new = pd.merge(df_metrics,df_neuron_percentage[['p_neurons','Internal_donor_ID']],on='Internal_donor_ID',how='left')
     
-    #remove row if donor_ID_python is nan
-    df_metrics_new = df_metrics_new.dropna(subset=['donor_ID_python'])
-    df_metrics_new.to_excel(path_output+'T2_quality_metrics_per_sample.xlsx',index=False)
+    #remove row if Internal_donor_ID is nan
+    df_metrics_new = df_metrics_new.dropna(subset=['Internal_donor_ID'])
+    df_metrics_new.to_excel(path_output+'T3_quality_metrics_per_sample.xlsx',index=False)
 else:    
-    #remove row if donor_ID_python is nan
-    df_metrics = df_metrics.dropna(subset=['donor_ID_python'])
-    df_metrics.to_excel(path_output+'T2_quality_metrics_per_sample.xlsx',index=False)
+    #remove row if Internal_donor_ID is nan
+    df_metrics = df_metrics.dropna(subset=['Internal_donor_ID'])
+    df_metrics.to_excel(path_output+'T3_quality_metrics_per_sample.xlsx',index=False)
